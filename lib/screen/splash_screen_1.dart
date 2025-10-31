9c2c6c578d8bf7237d49b4da89ed928acb150604
Asis Dwi Saputra <asisdwisaputra92@gmail.com>
2025-10-31 20:48:01 +0700

    latihan UTS Step 7

--- lib/screen/splash_screen_1.dart
+++ lib/screen/splash_screen_1.dart
@@ -6,25 +6,29 @@ class MySplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        //Todo
-        //posisi semua ditengah
-        children: [
-          // buat gambar
-          Container(
-            height: 200,
-            width: 200,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.lightBlue,
+      body: Center(
+        child: Column(
+          //Todo
+          //posisi semua ditengah
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // buat gambar
+            SizedBox(height: 20),
+            Container(
+              height: 200,
+              width: 200,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.lightBlue,
+              ),
             ),
-          ),
-        ],
-        // Buat Gambar
-        // title warna hitam
-        // subtitle warna lebih kecil
-        // ada baris bergambar bulet
-        // button
+          ],
+          // Buat Gambar
+          // title warna hitam
+          // subtitle warna lebih kecil
+          // ada baris bergambar bulet
+          // button
+        ),
       ),
     );
   }
