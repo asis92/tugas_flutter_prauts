8acb127d323c5dfaae47c25ed836da7217b52cfe
Asis Dwi Saputra <asisdwisaputra92@gmail.com>
2025-10-31 20:23:57 +0700

    latihan UTS step 4

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow),
       ),
+      home: MySplashScreen(),
     );
   }
 }
