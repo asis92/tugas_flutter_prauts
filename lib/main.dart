7deab403a8e2667a01b3a0a3ae2ce9a40aa28a75
Asis Dwi Saputra <asisdwisaputra92@gmail.com>
2025-10-31 20:23:21 +0700

    latihan UTS step 3

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      title: "Nim : 123456",
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow),
+      ),
+    );
   }
 }
