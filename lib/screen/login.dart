5d55e52462d4084b98af11a10f763a3d4da216f6
Asis Dwi Saputra <asisdwisaputra92@gmail.com>
2025-11-02 12:52:18 +0700

    latihan UTS Step 12

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -5,6 +5,19 @@ class MyWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(20.0),
+          child: const Column(
+            children: [
+              SizedBox(height: 10),
+              Icon(Icons.lock_outline, size: 70, color: Colors.lightBlue),
+              SizedBox(height: 10),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
